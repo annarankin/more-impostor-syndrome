8645d333976e4f385a62397175fda32bf404825e
annarankin <anniesqueedle@gmail.com>
2021-03-05 16:13:08 -0500

    Gets a test PR-creating action set up

--- pr-creation/index.js
+++ pr-creation/index.js
@@ -1,9 +1,28 @@
 (async () => {
-  const actionsClient = require('@actions/http-client')
-  const client = new actionsClient.HttpClient()
-  const res = await client.getJson('https://api.github.com/repos/annarankin/more-impostor-syndrome', {
-    'Accept': 'application/vnd.github.v3+json',
-    'User-Agent': 'annarankin'
+  const core = require('@actions/core');
+  const github = require('@actions/github');
+  const simpleGit = require('simple-git');
+  const fs = require('fs')
+
+  const myToken = core.getInput('myToken');
+  const octokit = github.getOctokit(myToken)
+  const git = simpleGit();
+
+  const now = new Date().getTime()
+  const branchname = `beep-boop-${now}`
+
+  await git.checkoutBranch(branchname, '5fc2416')
+  fs.writeFileSync(`./${branchname}.txt`, "HALLO WORLED")
+  await git.add([`./${branchname}.txt`])
+  await git.commit('Testing creating a commit')
+  await git.push('origin', branchname)
+
+  octokit.pulls.create({
+    owner: 'annarankin',
+    repo: 'more-impostor-syndrome',
+    title: `Test PR: ${branchname}`,
+    head: branchname,
+    base: 'main',
+    body: 'This is a test, this is only a test'
   })
-  console.log(res)
 })()
